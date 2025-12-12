7c01988526e4910baa37ad795681a208ae873857
Febrian1202 <febriansyahrian25@gmail.com>
2025-12-13 01:29:45 +0800

    Belajar dart

---
+++ bin/variable.dart
@@ -0,0 +1,18 @@
+void main() {
+
+    final var name = "12 + 5";
+
+    print(name);
+    print(name);
+
+    name = "Febrian";
+
+    print(name);
+    print(name);
+
+    var firstName = 'Eko';
+    final lastname = "Khannedy"; // Final tidak bisa dideklarasi ulang
+
+    print(firstName);
+    print(lastName);
+}
